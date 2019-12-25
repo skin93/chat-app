6cef5c7254f62034174dbce550d7b9c701698f20
skin93 <arro.ciesielski@gmail.com>
2019-12-25 10:58:11 +0100

    InfoBar component added

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 import './Chat.css';
+import InfoBar from '../InfoBar/InfoBar';
 
 let socket;
 
@@ -44,7 +45,8 @@ const Chat = ( { location } ) => {
   return (
     <div className="outerContainer">
       <div className="container">
-        <input value={ message } onChange={ ( e ) => setMessage( e.target.value ) } onKeyPress={ ( e ) => e.key === 'Enter' ? sendMessage( e ) : null } />
+        <InfoBar room={ room } />
+        {/* <input value={ message } onChange={ ( e ) => setMessage( e.target.value ) } onKeyPress={ ( e ) => e.key === 'Enter' ? sendMessage( e ) : null } /> */ }
       </div>
     </div>
   );
